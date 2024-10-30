94a4865b52e80d72757482a646cf17d02a7255d6
Galymbek <galymbek.batyrbekk@gmail.com>
2024-10-30 16:12:42 +0500

    changed file Javascript

--- hw3-week9-JS/js/script.js
+++ hw3-week9-JS/js/script.js
@@ -1,1 +1,48 @@
-console.log("Сыртқы байланыс");
+// console.log("Сыртқы байланыс");
+
+
+//Айнымалылардын жариялануы
+var name = '5';
+var age = 15;
+var isStudent = false; // boolean true/false
+
+var a;  // анықталмаған 
+
+
+if(isStudent){
+
+}
+
+console.log("Name + age = " + (age*name));
+
+
+console.log("a дін мәні:" + a);
+console.log(typeof name); // string
+console.log(typeof age); //number
+
+
+
+
+
+
+
+// if(true){
+//     let age = 20
+//     console.log("Жасы: " + age);   
+// }
+
+    var firstNum = document.getElementById('firstNum').innerHTML
+    var secondNum = document.getElementById('secondNum').innerHTML
+
+    var sum=null;
+
+    sum = firstNum + secondNum
+
+    console.log("First: " + firstNum);
+    console.log("Second: " + secondNum);
+
+    console.log("Ответ: " +sum );
+    
+
+console.log("Жасы: " + age); 
+
