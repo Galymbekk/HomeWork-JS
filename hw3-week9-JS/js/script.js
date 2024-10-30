cdf770ea0994ae6f29f73f3d7c71dfd836295ad2
Galymbek <galymbek.batyrbekk@gmail.com>
2024-10-30 18:11:25 +0500

    change title

--- hw3-week9-JS/js/script.js
+++ hw3-week9-JS/js/script.js
@@ -1,48 +1,69 @@
-// console.log("Сыртқы байланыс");
+var name= 'Abdurakhman'
+
+var ernar;
+
+console.log(typeof name);
+
+console.log(typeof ernar);
 
 
-//Айнымалылардын жариялануы
-var name = '5';
-var age = 15;
-var isStudent = false; // boolean true/false
-
-var a;  // анықталмаған 
+console.log("Ernardyn mani: " + ernar);
 
 
-if(isStudent){
+var name = 25 //number
 
+name.toString() //number -> string
+
+console.log("Name ді number дан текстке ауыстрыдык " + typeof name);
+
+var durys = true;
+var qate = false;
+
+
+console.log(typeof durys);
+
+var natizhe = null; //мани бос
+console.log("Natizhe: " + natizhe);
+
+console.log(typeof natizhe);
+
+var san1 = '15'
+var san2 = 15
+
+natizhe = san1 + san2 // 10 + 15
+console.log("Бақжаннын нәтижесі:  " + natizhe);
+
+console.log(typeof natizhe);
+
+
+
+if(5 + 5 == 7){
+    console.log(durys);
+}
+else{
+    console.log(qate);    
 }
 
-console.log("Name + age = " + (age*name));
+// const name = "Bakzhan"
 
+let Bakzhan = 5;
 
-console.log("a дін мәні:" + a);
-console.log(typeof name); // string
-console.log(typeof age); //number
-
-
-
-
-
-
-
-// if(true){
-//     let age = 20
-//     console.log("Жасы: " + age);   
-// }
-
-    var firstNum = document.getElementById('firstNum').innerHTML
-    var secondNum = document.getElementById('secondNum').innerHTML
-
-    var sum=null;
-
-    sum = firstNum + secondNum
-
-    console.log("First: " + firstNum);
-    console.log("Second: " + secondNum);
-
-    console.log("Ответ: " +sum );
+if(true){
+    let Bakzhan=10;
+    console.log("a: " + Bakzhan);
     
+}
+console.log("a: " + Bakzhan);
 
-console.log("Жасы: " + age); 
 
+const text= "Сіздің паролиниз немесе логин кате"
+
+const PI = 3.14;
+
+PI=2.15
+
+// PI= 3.25
+
+// name = "Galymbek"
+
+console.log("PI: " + PI);
