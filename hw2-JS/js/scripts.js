6a1c14603f53298d1b71327266029519828d91c5
Galymbek <galymbek.batyrbekk@gmail.com>
2024-10-30 10:16:08 +0500

    Variables and data types

---
+++ hw2-JS/js/scripts.js
@@ -0,0 +1,33 @@
+// let name = "Amjilt Cyber School"
+
+// if(true){
+//    let name = "Galymbek"
+//    console.log("Условиянын ишинде аты: " + name);   
+// }
+
+/* Деректер түрлері */
+
+const name="Galymbek"
+let age //underfined
+let age2='10'
+
+let isGalymbek = false
+
+ 
+
+
+var scoreRealBarcelona = "0 : 4"
+
+var myName;
+
+
+// name="Efrat"
+
+// console.log("Менин атым: " + name);
+// console.log("My age is: ", age);
+console.log(myName);
+console.log(age);
+
+
+ 
+
