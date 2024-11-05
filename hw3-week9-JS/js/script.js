63a0021dbc520b4d1f5b7429d52d6d794eaada78
Galymbek <galymbek.batyrbekk@gmail.com>
2024-11-05 10:53:10 +0500

    prompt added

--- hw3-week9-JS/js/script.js
+++ hw3-week9-JS/js/script.js
@@ -1,69 +1,73 @@
-var name= 'Abdurakhman'
+let a=9, b=10, c=12;
+let isStudent = true;
+let isAlumni =true; //выпускник немесе түлек
+let san = 0; //false
 
-var ernar;
+console.log("San: ", !san); //San: true
 
-console.log(typeof name);
-
-console.log(typeof ernar);
-
-
-console.log("Ernardyn mani: " + ernar);
-
-
-var name = 25 //number
-
-name.toString() //number -> string
-
-console.log("Name ді number дан текстке ауыстрыдык " + typeof name);
-
-var durys = true;
-var qate = false;
-
-
-console.log(typeof durys);
-
-var natizhe = null; //мани бос
-console.log("Natizhe: " + natizhe);
-
-console.log(typeof natizhe);
-
-var san1 = '15'
-var san2 = 15
-
-natizhe = san1 + san2 // 10 + 15
-console.log("Бақжаннын нәтижесі:  " + natizhe);
-
-console.log(typeof natizhe);
-
-
-
-if(5 + 5 == 7){
-    console.log(durys);
+if(a=""){
+    console.log("Бос жол");
 }
 else{
-    console.log(qate);    
+    console.log("а-да мән бар");
 }
 
-// const name = "Bakzhan"
+console.log(!isStudent); //false
 
-let Bakzhan = 5;
 
-if(true){
-    let Bakzhan=10;
-    console.log("a: " + Bakzhan);
-    
+if(!isStudent && !isAlumni){
+    console.log("Сізге мектеп қабырғасына кіруге болмайды");
 }
-console.log("a: " + Bakzhan);
+else if(isStudent || isAlumni){
+    console.log("Сізге зеленый цвет!");
+}
+
+// == (тең)
+console.log(a==b); // true
+
+//!= (тең емес)
+console.log(a!=b); //false
+
+//>
+console.log(a>b); //false
+
+//<
+console.log(a<b); //false
+
+//<=(үлкен немесе тең)
+console.log(a>=b); // 9>=10 false
+
+//>=(кіші немесе тең)
+console.log(a<=b);// 9<=10 true
+
+// &&(және) екі шарттың екеуі де true болған жағдайда, true қайтарады
+console.log((a>b) && (c>a)); // 9>10(false) 12>9(true) => false
+
+// || (немесе) екі шарттың кемінде біреуі ғана true болған жағдайда, true қайтарады
+console.log((a>b) || (c>a)); // 9>10 (false) 12>9(true) => true
 
 
-const text= "Сіздің паролиниз немесе логин кате"
+//Күрделі өрнек
+let age=20, hasTicket = true, isVip = false;
 
-const PI = 3.14;
+if((age>=18 && hasTicket) || isVip){
+    console.log("Сізге кешке кіруге болады!");
+}
+else{
+    console.log("Сізге кешке кіруге тыйым салынады!");
+}
 
-PI=2.15
+//Нәтиже: Cізге кешке кіруге болады!
 
-// PI= 3.25
+// 12<=x<=45 Практика 1
+let agee = 11
 
-// name = "Galymbek"
+// alert((agee>=12) && (agee<=45));
 
-console.log("PI: " + PI);
+// 20<=x<=50 true Практика 2 
+
+let n=21
+
+alert((n>=20) && (n<=50) && (n % 2==0))
+
+
